c860af2d12343d44b7f65ba607946d044ea88c24
najimattar <160213152+najimattar@users.noreply.github.com>
2026-09-26 12:04:59 +0530

    Update trivy.sh

--- docker/my-ecommerce/trivy.sh
+++ docker/my-ecommerce/trivy.sh
@@ -1,5 +1,2 @@
-sudo apt-get install wget gnupg -y
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | gpg --dearmor | sudo tee /usr/share/keyrings/trivy.gpg > /dev/null
-echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.io/trivy-repo/deb generic main" | sudo tee -a /etc/apt/sources.list.d/trivy.list
-sudo apt-get update
-sudo apt-get install trivy -y
+
+curl -sfL https://raw.githubusercontent.com/aquasecurity/trivy/main/contrib/install.sh | sudo sh -s -- -b /usr/local/bin v0.74.0
